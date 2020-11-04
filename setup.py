b81d36d8b65e2aee9d9a3dd7565144c420c80f79
ozgurkara <ozgurkara85@gmail.com>
2020-11-04 18:30:09 +0300

    setup name changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pydiator_core",
+    name="pydiator-core",
     version="0.0.1",
     author="Özgür Kara",
     author_email="ozgurkara85@gmail.com",
