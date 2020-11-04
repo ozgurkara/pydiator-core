818bfb52a492f0b71165203f9aba1f22cf8cab48
ozgurkara <ozgurkara85@gmail.com>
2020-11-04 17:52:23 +0300

    first commit

---
+++ pydiator_core/pipelines/log_pipeline.py
@@ -0,0 +1,27 @@
+from typing import List
+from pydiator_core.interfaces import BaseRequest, BasePipeline
+from pydiator_core.serializer import Serializer
+
+
+class LogPipeline(BasePipeline):
+    async def handle(self, req: BaseRequest) -> object:
+        print(f"LogPipeline:handle:{type(req).__name__}")
+
+        if self.next() is None:
+            raise Exception("pydiator_log_pipeline_next_error")
+
+        response = await self.next().handle(req)
+
+        if hasattr(response, "__dict__") or isinstance(response, List):
+            _response = Serializer.deserialize(response)
+        else:
+            _response = str(response)
+
+        log_obj = {
+            "req": Serializer.deserialize(req),
+            "res": _response
+        }
+
+        print("log", log_obj)
+
+        return response
