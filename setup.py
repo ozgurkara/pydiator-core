632559d8e2768063213382f836882936dc04f1cb
özgür kara <ozgurkara85@gmail.com>
2020-12-12 00:32:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pydiator-core",
-    version="1.0.2",
+    version="1.0.3",
     author="Özgür Kara",
     author_email="ozgurkara85@gmail.com",
     description="Pydiator",
